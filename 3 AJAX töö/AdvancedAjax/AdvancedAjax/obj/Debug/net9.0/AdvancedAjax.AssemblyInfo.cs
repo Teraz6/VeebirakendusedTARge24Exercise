c921d78f412a837849d1d16c6f284d0778c8cb43
Teraz6 <retenk@gmail.com>
2025-05-14 16:23:01 +0300

    Create views for CityController

--- 3 AJAX töö/AdvancedAjax/AdvancedAjax/obj/Debug/net9.0/AdvancedAjax.AssemblyInfo.cs
+++ 3 AJAX töö/AdvancedAjax/AdvancedAjax/obj/Debug/net9.0/AdvancedAjax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9a45832488208911d6ae7718468a1d03cf8360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ffdaa7dcf680e73d7d359864c6ac89f058cf43")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
