020438bc350d02c21a1cab14138e5104013c31fc
Teraz6 <retenk@gmail.com>
2025-05-14 15:39:59 +0300

    Create Views for CRUD

--- 3 AJAX töö/AdvancedAjax/AdvancedAjax/obj/Debug/net9.0/AdvancedAjax.AssemblyInfo.cs
+++ 3 AJAX töö/AdvancedAjax/AdvancedAjax/obj/Debug/net9.0/AdvancedAjax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43e1fad4ca139890f18d5a67923b94345ef86e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9a45832488208911d6ae7718468a1d03cf8360")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
