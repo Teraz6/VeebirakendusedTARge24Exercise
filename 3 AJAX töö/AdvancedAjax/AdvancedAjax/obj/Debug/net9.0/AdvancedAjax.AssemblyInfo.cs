86a2395b6e35c3835c55697d4fc622cd49bff61f
Teraz6 <retenk@gmail.com>
2025-05-17 14:22:11 +0300

    Add MISC files

--- 3 AJAX töö/AdvancedAjax/AdvancedAjax/obj/Debug/net9.0/AdvancedAjax.AssemblyInfo.cs
+++ 3 AJAX töö/AdvancedAjax/AdvancedAjax/obj/Debug/net9.0/AdvancedAjax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8022f495f262e1b801f5c2c34f4510864f0b049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3189491e32536fb3e8f12ad23b7b7b4df1f2020")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
